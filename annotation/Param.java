3728fee16f3064828b2a59d69e40befb07376473
AndoraRavelo <andoniainarandrianarivelo28@gmail.com>
2025-12-21 09:24:56 +0300

    Update Sprint8

--- annotation/Param.java
+++ annotation/Param.java
@@ -1,4 +1,4 @@
-package annotation;
+package framework.annotation;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -6,11 +6,11 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Annotation to bind a URI template variable to a method parameter.
- * Example: @GetMapping("/produits/{id}") and method parameter @PathVariable("id") int id
- *////sprint 6 ter
+ * Annotation to bind a request parameter (or path variable) to a method parameter.
+ * Usage: @Param("id") int idObjet
+ */
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface PathVariable {
+public @interface Param {
     String value();
 }
